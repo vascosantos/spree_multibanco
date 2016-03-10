ae7d8f8b06b395e5b2a724b4536b4a73acb2f770
Vasco Santos <vasco.f.santos@gmail.com>
2016-03-10 17:25:16 +0000

    kim

--- app/overrides/add_multibanco_providers_to_admin_configuration_sidebar.rb
+++ app/overrides/add_multibanco_providers_to_admin_configuration_sidebar.rb
@@ -3,6 +3,6 @@ Deface::Override.new(
   :name => 'add_multibanco_providers_to_admin_configuration_sidebar',
   :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
   :text => %q{
-    <%= configurations_sidebar_menu_item(Spree.t(:multibanco), spree.admin_multibanco_providers_path) if can? :manage, Spree::PaymentMethod %>
+    <%= configurations_sidebar_menu_item(Spree.t(:multibanco_settings), spree.admin_multibanco_providers_path) if can? :manage, Spree::PaymentMethod %>
     }
 )
