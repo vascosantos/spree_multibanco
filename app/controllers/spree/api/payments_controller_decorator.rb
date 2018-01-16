1500fbb3bb594d6ac639b01717e4ebd1a113b58d
Vasco Santos <vasco.f.santos@gmail.com>
2018-01-16 19:17:12 +0000

    add create invoice and send paid email logic

--- app/controllers/spree/api/payments_controller_decorator.rb
+++ app/controllers/spree/api/payments_controller_decorator.rb
@@ -9,7 +9,19 @@ Spree::Api::PaymentsController.class_eval do
     if @payment && params[:referencia] == @payment.multibanco_reference && params[:valor].to_d == @payment.amount && params[:entidade] == @multibanco_provider.entity
       @order.resume! if @order.state == "canceled"
       capture
-      @order.send_paid_email if @order.respond_to?(:send_paid_email)
+      if Spree::Config.has_preference?(:automatic_invoices) && Spree::Config[:automatic_invoices]
+        ## Create invoice and send paid email
+        if defined?(NewCreateInvoiceJob) == 'constant' && NewCreateInvoiceJob.class == Class  
+          email_proc = Proc.new { @order.send_paid_email if @order.respond_to?(:send_paid_email) }
+          NewCreateInvoiceJob.perform_later(@order, email_proc) 
+        else
+          ## Only send paid email
+          @order.send_paid_email if @order.respond_to?(:send_paid_email)
+        end
+      else
+        ## Only send paid email
+        @order.send_paid_email if @order.respond_to?(:send_paid_email)
+      end
     else
       not_found
     end
